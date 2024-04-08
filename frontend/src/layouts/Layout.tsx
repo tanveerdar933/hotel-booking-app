ac4715976c9c844e47006f0443c1af4bbe650fc7
Muhammad Tanveer Anjum <tanveerdar933@gmail.com>
2024-04-08 23:18:34 +0500

    fixed import

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
