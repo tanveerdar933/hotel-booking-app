9612e570da4a25702a0844c2816422ff93d4308c
Muhammad Tanveer Anjum <tanveerdar933@gmail.com>
2024-04-09 01:43:59 +0500

    added login and register routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,21 +2,23 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 const PORT = 8000;
 
-mongoose.connect(process.env.MONGO_DB_URI as string)
-  .then(() => console.log("MongoDB connection established successfully!"))
+mongoose
+  .connect(process.env.MONGO_DB_URI as string)
+  .then(() => console.log("MongoDB connection established successfully!"));
 
 const app = express();
 
 app.use(express.json()); //help convert every req body into json
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello from express endpoint!" })
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on localhost:", PORT);
-})
+});
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,57 @@
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+const router = express.Router();
+
+router.post(
+  "/register",
+  [
+    check("email", "Email is required").isString(),
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    try {
+      let user = await User.findOne({
+        email: req.body.email,
+      });
+
+      if (user) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      user = new User({ ...req.body });
+      await user.save();
+
+      const token = jwt.sign(
+        {
+          userId: user._id,
+        },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000, //in milliseconds => same value as the jwt expiry
+      });
+
+      res.sendStatus(200);
+    } catch (error) {
+      res.status(500).send({ message: "something went wrong." });
+    }
+  }
+);
+
+export default router;
