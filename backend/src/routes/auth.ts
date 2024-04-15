03d47ac725cce2b3823aa42f50f8438c61d0ff09
Muhammad Tanveer Anjum <tanveerdar933@gmail.com>
2024-04-15 21:11:56 +0500

    added logout route

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -57,4 +57,13 @@ router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
   res.status(200).send({ userId: req.userId });
 });
 
+router.post("/logout", (req: Request, res: Response) => {
+  res.cookie("auth_token", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    expires: new Date(0),
+  });
+  res.status(200).send({ message: "Logout successfully" });
+});
+
 export default router;
